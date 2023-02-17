638d60d2753f11b009b9ca7b9ee6df11f8be3a76
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-02-17 18:14:43 -0500

    Temporary print statement instrumentation.

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -3,5 +3,9 @@ int foo(int a, int b) { return a + b; }
 int main() {
   int a = 0, b = 1;
   int c = foo(a, b);
-  return (c == 1) ? 0 : 1;
+  if (c == 1) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
--- src/functioncall_transformer.cpp
+++ src/functioncall_transformer.cpp
@@ -15,13 +15,8 @@ FunctionCallTransformer::FunctionCallTransformer(clang::ASTContext &context,
 void FunctionCallTransformer::start(void) {
   clang::ast_matchers::MatchFinder function_finder;
 
-  auto call_expr_matcher = clang::ast_matchers::callExpr().bind("callExpr");
-  function_finder.addMatcher(call_expr_matcher, this);
-
   // Add matcher for selection statements.
-  auto selection_statement_matcher =
-      clang::ast_matchers::stmt(clang::ast_matchers::anyOf(
-          clang::ast_matchers::ifStmt(), clang::ast_matchers::switchStmt()));
+  auto selection_statement_matcher = clang::ast_matchers::ifStmt().bind("ifStmt");
   function_finder.addMatcher(selection_statement_matcher, this);
 
   function_finder.matchAST(context);
@@ -33,19 +28,7 @@ void FunctionCallTransformer::run(
   if (const clang::IfStmt *if_stmt =
           result.Nodes.getNodeAs<clang::IfStmt>("ifStmt")) {
     spdlog::warn("Found if statement.");
-  }
-
-  // CallExpr is the function call
-  // FunctionDecl is fhe function definition
-  if (const clang::CallExpr *call_expr =
-          result.Nodes.getNodeAs<clang::CallExpr>("callExpr")) {
-    if (const clang::FunctionDecl *function = call_expr->getDirectCallee()) {
-      if (result.SourceManager->isInSystemHeader(
-              function->getSourceRange().getBegin()))
-        return;
-
-      auto function_name = function->getNameAsString();
-      rewriter.InsertTextAfter(call_expr->getBeginLoc(), "fn_");
-    }
+    static std::string print_stmt = "std::cout << \"hello\" << std::endl; ";
+    rewriter.InsertTextBefore(if_stmt->getBeginLoc(), print_stmt);
   }
 }
