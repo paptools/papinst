508313c05f968112ebc9c6bdf5991cdeeceacbab
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-04-20 00:10:55 -0400

    Temporary change to debug github container.

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -5,4 +5,9 @@ find_package(Clang REQUIRED CONFIG)
 
 include_directories(${LLVM_INCLUDE_DIRS})
 
+message(STATUS "LLVM include dirs: ${LLVM_INCLUDE_DIRS}")
+message(STATUS "Clang include dirs: ${CLANG_INCLUDE_DIRS}")
+message(STATUS "LLVM definitions: ${LLVM_DEFINITIONS}")
+message(STATUS "Clang definitions: ${CLANG_DEFINITIONS}")
+
 message(STATUS "Found Clang ${LLVM_PACKAGE_VERSION}")
