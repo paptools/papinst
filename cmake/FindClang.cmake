297b1c3a6f9b404c10d8d9c52c272d77fe609ddf
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-04-20 00:03:13 -0400

    Attempting to fix interface includes error.

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -3,4 +3,6 @@ message(STATUS "Finding Clang")
 find_package(LLVM REQUIRED CONFIG)
 find_package(Clang REQUIRED CONFIG)
 
+include_directories(${LLVM_INCLUDE_DIRS})
+
 message(STATUS "Found Clang ${LLVM_PACKAGE_VERSION}")
