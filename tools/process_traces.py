66645d1054c040a5f05695b4a639b3d603d9bf2b
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-07-11 01:25:05 -0400

    Added complexity for operators.

--- tools/process_traces.py
+++ tools/process_traces.py
@@ -128,38 +128,24 @@ def main():
     # It is expected that we start with some known values.
     # NOTE: This currently uses signatures to generalize (i.e., it is not accounting for context).
     known = {
-        # "int operator-(unsigned short, int)": ["C_OP_INT_SUB"],
-        # "int operator+(unsigned short, int)": ["C_OP_INT_ADD"],
-        # "value_type operator=(value_type, int)": ["C_OP_INT_ASSIGN"],
-        # "reference std::vector<unsigned long long>::operator[](size_type)": [
-        #    "C_OP_VEC_SUBSCRIPT"
-        # ],
-        # "value_type operator=(value_type, unsigned long long)": [
-        #    "C_OP_VEC_ASSIGN"
-        # ],
-        # "unsigned long long fibonacci::(anonymous namespace)::RecursiveMemoImpl(unsigned short, std::vector<unsigned long long> &)": [
-        #    "C_FIB_RECURSIVE_MEMO_IMPL"
-        # ],
-        # "unsigned long long operator+(unsigned long long, unsigned long long)": [
-        #    "C_OP_ULL_ADD"
-        # ],
-        # "unsigned long long operator=(unsigned long long, unsigned long long)": [
-        #    "C_OP_ULL_ASSIGN"
-        # ],
-        "int operator-(unsigned short, int)": [],
-        "int operator+(unsigned short, int)": [],
-        "value_type operator=(value_type, int)": [],
-        "reference std::vector<unsigned long long>::operator[](size_type)": [],
-        "value_type operator=(value_type, unsigned long long)": [],
-        "unsigned long long fibonacci::(anonymous namespace)::RecursiveMemoImpl(unsigned short, std::vector<unsigned long long> &)": (
-            []
-        ),
-        "unsigned long long operator+(unsigned long long, unsigned long long)": (
-            []
-        ),
-        "unsigned long long operator=(unsigned long long, unsigned long long)": (
-            []
-        ),
+        "int operator-(unsigned short, int)": ["C_OP_INT_SUB"],
+        "int operator+(unsigned short, int)": ["C_OP_INT_ADD"],
+        "value_type operator=(value_type, int)": ["C_OP_INT_ASSIGN"],
+        "reference std::vector<unsigned long long>::operator[](size_type)": [
+            "C_OP_VEC_SUBSCRIPT"
+        ],
+        "value_type operator=(value_type, unsigned long long)": [
+            "C_OP_VEC_ASSIGN"
+        ],
+        "unsigned long long fibonacci::(anonymous namespace)::RecursiveMemoImpl(unsigned short, std::vector<unsigned long long> &)": [
+            "C_FIB_RECURSIVE_MEMO_IMPL"
+        ],
+        "unsigned long long operator+(unsigned long long, unsigned long long)": [
+            "C_OP_ULL_ADD"
+        ],
+        "unsigned long long operator=(unsigned long long, unsigned long long)": [
+            "C_OP_ULL_ASSIGN"
+        ],
     }
     exprs = {}
     call_path_ids = {}
