90f8f8ae0ee94987e7cd77ba515a4643591acceb
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-07-12 00:07:32 -0400

    Added initial solver steps.

--- tools/process_traces.py
+++ tools/process_traces.py
@@ -99,6 +99,95 @@ def get_path_partitions(trees):
     return path_dict
 
 
+def get_expr_data(path_dict, known):
+    exprs = {}
+    call_path_ids = {}
+
+    # Expr data should map: sig->path_id->ctx->expr
+    expr_data = {}
+
+    def to_expr(node, level=0):
+        subexpr = []
+        for child in node.children:
+            subexpr.extend(to_expr(child, level + 1))
+        if node.is_root:
+            subexpr.append(
+                f"C_F{node.name}_P{call_path_ids[to_call_str(node)]}"
+            )
+        elif paptree.Node.is_call_type(node.type):
+            # print(f"{' ' * 2 * level}including node {to_simple_node_view(node)}")
+            if node.sig in known:
+                subexpr.extend(known[node.sig])
+            else:
+                subexpr.extend(exprs[to_call_str(node)])
+        else:
+            # print(f"{' ' * 2 * level}excluding node {to_simple_node_view(node)}")
+            pass
+        return subexpr
+
+    for sig, sig_paths in path_dict.items():
+        for _, path_data in sig_paths.items():
+            path_id = path_data["path_id"]
+            for tree in path_data["traces"]:
+                call_path_ids[to_call_str(tree.root)] = path_id
+                expr = to_expr(tree.root)
+                exprs[to_call_str(tree.root)] = expr
+                sig_data = expr_data.setdefault(tree.root.sig, {})
+                path_data = sig_data.setdefault(f"path_{path_id}", {})
+                param_str = to_params_str(tree.root.params)
+                # with sympy.evaluate(False):
+                sympy_expr = sympy.sympify(" + ".join(expr))
+                path_data[param_str] = sympy.srepr(sympy_expr)
+    return expr_data
+
+
+def solve(path_exprs):
+    # If there is only one expression, return it.
+    if len(path_exprs) == 1:
+        return list(path_exprs.values())[0]
+    # Check if all expressions are equal. If they are, return the expression
+    # since it is constant.
+    expr_set = set([e for _, e in path_exprs.items()])
+    if len(expr_set) == 1:
+        return expr_set.pop()
+    else:
+        print("\nUnsolved exprs:")
+        for expr in expr_set:
+            print(expr)
+    return None
+
+
+def solve_exprs(expr_data):
+    # Solve for each path.
+    # We will want to query the results with a tuple of (signature, ctx). To do
+    # this we will need to map the ctx for a signature to the correct path ID.
+    # Therefore, the results will need to contain two sections:
+    # 1. A mapping from (signature) to sig_id.
+    # 2. A mapping from (sig_id, ctx) to path_id.
+    # 3. A mapping from (sig_id, path_id) to the path expression.
+    results = {"sigs": {}, "ctxs": {}, "exprs": {}}
+
+    skip_sigs = ["unsigned long long fibonacci::RecursiveMemo(unsigned short)"]
+    for sig, sig_data in expr_data.items():
+        if sig in skip_sigs:
+            continue
+        sig_id = results["sigs"].setdefault(sig, f"sig_{len(results['sigs'])}")
+        for path_id, path_data in sig_data.items():
+            path_exprs = {}
+            for ctx, expr in path_data.items():
+                sexpr = sympy.sympify(expr)
+                path_exprs[ctx] = sexpr
+            result = solve(path_exprs)
+            if result:
+                results["exprs"].setdefault(sig_id, {})[path_id] = sympy.srepr(
+                    result
+                )
+                result_ctxs = results["ctxs"].setdefault(sig_id, {})
+                for ctx in path_exprs.keys():
+                    result_ctxs[ctx] = path_id
+    return results
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="input file")
@@ -146,49 +235,16 @@ def main():
             "C_OP_ULL_ASSIGN"
         ],
     }
-    exprs = {}
-    call_path_ids = {}
 
-    # Expr data should map: sig->path_id->ctx->expr
-    expr_data = {}
-
-    def to_expr(node, level=0):
-        subexpr = []
-        for child in node.children:
-            subexpr.extend(to_expr(child, level + 1))
-        if node.is_root:
-            subexpr.append(
-                f"C_F{node.name}_P{call_path_ids[to_call_str(node)]}"
-            )
-        elif paptree.Node.is_call_type(node.type):
-            # print(f"{' ' * 2 * level}including node {to_simple_node_view(node)}")
-            if node.sig in known:
-                subexpr.extend(known[node.sig])
-            else:
-                subexpr.extend(exprs[to_call_str(node)])
-        else:
-            # print(f"{' ' * 2 * level}excluding node {to_simple_node_view(node)}")
-            pass
-        return subexpr
-
-    for sig, sig_paths in path_dict.items():
-        for _, path_data in sig_paths.items():
-            path_id = path_data["path_id"]
-            for tree in path_data["traces"]:
-                call_path_ids[to_call_str(tree.root)] = path_id
-                expr = to_expr(tree.root)
-                exprs[to_call_str(tree.root)] = expr
-                sig_data = expr_data.setdefault(tree.root.sig, {})
-                path_data = sig_data.setdefault(f"path_{path_id}", {})
-                param_str = to_params_str(tree.root.params)
-                # with sympy.evaluate(False):
-                sympy_expr = sympy.sympify(" + ".join(expr))
-                path_data[param_str] = sympy.srepr(sympy_expr)
-
+    expr_data = get_expr_data(path_dict, known)
     print("\nProcessed Traces:")
     print(json.dumps(expr_data, indent=2))
+
+    results = solve_exprs(expr_data)
+    print("\nResults:")
+    print(json.dumps(results, indent=4))
     with open(args.output_file, "w") as f_out:
-        f_out.write(json.dumps(expr_data, indent=2))
+        f_out.write(json.dumps(results, indent=2))
 
 
 if __name__ == "__main__":
