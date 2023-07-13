4c5de04ea035ddf7cdb2af8dfdec2d0009cff7ee
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-07-12 22:16:37 -0400

    Added perf test for factorial fn.

--- experiments/demo/tests/test_demo.py
+++ experiments/demo/tests/test_demo.py
@@ -27,9 +27,25 @@ def get_expr(data, sig, ctx):
     return sympy.sympify(expr)
 
 
-def test_is_even():
+def test_is_even_constant():
     sig = "_Bool demo::IsEven(int)"
+    # All paths are constant.
     ctxs = ["0", "1", "2", "3", "4", "5"]
     for ctx in ctxs:
         expr = get_expr(pytest.expr_data, sig, ctx)
         assert expr in get_constant(n)
+
+
+def test_factorial():
+    sig = "int demo::Factorial(int)"
+    # Paths for x < 1 and x > 31 are constant.
+    ctxs = ["0", "32"]
+    for ctx in ctxs:
+        expr = get_expr(pytest.expr_data, sig, ctx)
+        assert expr in get_constant(n)
+    # Paths for 1 <= x <= 31 are linear.
+    ctxs = ["1", "2", "3"]
+    for ctx in ctxs:
+        expr = get_expr(pytest.expr_data, sig, ctx)
+        assert expr in get_linear(n)
+        assert expr not in get_constant(n)
